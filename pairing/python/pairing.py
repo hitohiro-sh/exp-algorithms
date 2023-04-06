ad4791a2ad922f1fdd0cc65e62165c4f04239fce
Hitohiro Shiozaki <h.shiozaki.it.just@gmail.com>
2023-04-06 15:44:04 +0900

    remove parent. fix meld.

--- pairing/python/pairing.py
+++ pairing/python/pairing.py
@@ -9,12 +9,10 @@ T = TypeVar('T', int, float, str, Tuple[Self, ...])
 class Node(Generic[T]):
     val: T
     childs: List[Self] = field(default_factory=list)
-    parent: Optional[Self] = None
 
 
     def addChild(self, node : Self):
         self.childs.append(node)
-        node.parent = self
 
 
     def print(self, d=0, _p=print):
@@ -28,18 +26,31 @@ class Node(Generic[T]):
 class PairingHeap(Generic[T]):
     root: Node = None
 
-    def _meld(self, root: Node[T]):
-        if self.root.val < root.val:
-            self.root.addChild(root)
+    
+
+    @classmethod
+    def meld(self, node1: Node[T], node2: Node[T]) -> Node[T]:
+        if node1.val < node2.val:
+            node1.addChild(node2)
+            return node1
         else:
-            root.addChild(self.root)
-            self.root = root
+            node2.addChild(node1)
+            return node2
+        
+    #def _meld(self, root: Node[T]):
+
+        #if self.root.val < root.val:
+        #    self.root.addChild(root)
+        #else:
+        #    root.addChild(self.root)
+        #    self.root = root
 
     def add(self, v: T):
         if not self.root:
             self.root = Node(v)
         else:
-            self._meld(Node(v))
+            self.root = PairingHeap.meld(self.root, Node(v))
+            
 
     def pop(self) -> T:
         root = self.root
@@ -48,9 +59,21 @@ class PairingHeap(Generic[T]):
         if not self.root.childs:
             self.root = None
         else:
-            self.root = root.childs[0]
-            for c in root.childs[1:]:
-                self._meld(c)
+            trees = root.childs
+            tmp = []
+
+            while len(trees) > 1:
+                for i in range(0, len(trees), 2):
+                    if i+1 < len(trees):
+                        tmp.append(PairingHeap.meld(trees[i], trees[i+1]))
+                    else:
+                        tmp.append(trees[i])
+                trees = tmp
+                tmp = []
+            self.root = trees[0]
+            #self.root = root.childs[0]
+            #for c in root.childs[1:]:
+            #    self._meld(c)
 
         return val
     
@@ -91,6 +114,21 @@ def main():
     t.print()
     t.pop()
     t.print()
+
+    print('---')
+    import random
+    random.seed(1)
+
+    t = PairingHeap()
+    for _ in range(50):
+        i = random.randint(1, 50)
+        t.add(i * 10)
+        if random.randint(0, 10) < 3:
+            print('---')
+            t.print()
+            t.pop()
+            print('--- pop ---')
+            t.print()
 
     pass
 
